8546eb5e5579e4d114229da82ea741bf4a5591d2
rvterran <rvterran@gmail.com>
2019-02-17 14:45:35 +0300

    fix disabled

--- src/App.js
+++ src/App.js
@@ -1,61 +1,61 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 
 import Item from './item';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="container">
-            <h1 className="title">Ты сегодня покормил кота?</h1>
-            <div className="item-list">
-              <Item
-                headText="Сказочное заморское яство"
-                titleName="Нямушка"
-                titleNameWith="с фуа-гра"
-                subtext1={[<span className="bold">10</span>, " порций"]}
-                subtext2="мышь в подарок"
-                weightCount="0,5"
-                unitMeas="кг"
-                selectedHoverText="Котэ не одобряет?"
-                footerText={["Чего сидишь? Порадуй котэ, ", <span className='buy'>купи.</span>]}
-                footerTextSelected="Печень утки разварная с артишоками."
-                footerTextDisabled="Печалька, с фуа-гра закончился."
-              />
-              <Item
-                headText="Сказочное заморское яство"
-                titleName="Нямушка"
-                titleNameWith="с рыбой"
-                subtext1={[<span className="bold">40</span>, " порций"]}
-                subtext2={[<span className="bold">2</span>, " мыши в подарок"]}
-                weightCount="2"
-                unitMeas="кг"
-                selectedHoverText="Котэ не одобряет?"
-                footerText={["Чего сидишь? Порадуй котэ, ", <span className='buy'>купи.</span>]}
-                footerTextSelected="Головы щучьи с чесноком да свежайшая сёмгушка."
-                footerTextDisabled="Печалька, с рыбой закончился."
-              />
-              <Item
-                headText="Сказочное заморское яство"
-                titleName="Нямушка"
-                titleNameWith="с курой"
-                subtext1={[<span className="bold">100</span>, " порций"]}
-                subtext2={[<span className="bold">5</span>, " мышей в подарок"]}
-                subtext3="заказчик доволен"
-                weightCount="5"
-                unitMeas="кг"
-                selectedHoverText="Котэ не одобряет?"
-                footerText={["Чего сидишь? Порадуй котэ, ", <span className='buy' >купи.</span>]}
-                footerTextSelected="Филе из цыплят с трюфелями в бульоне."
-                footerTextDisabled="Печалька, с курой закончился."
-                disabled
-              />
+    render() {
+        return (
+            <div className="App">
+                <div className="container">
+                    <h1 className="title">Ты сегодня покормил кота?</h1>
+                    <div className="item-list">
+                        <Item
+                            headText="Сказочное заморское яство"
+                            titleName="Нямушка"
+                            titleNameWith="с фуа-гра"
+                            subtext1={[<span className="bold">10</span>, " порций"]}
+                            subtext2="мышь в подарок"
+                            weightCount="0,5"
+                            unitMeas="кг"
+                            selectedHoverText="Котэ не одобряет?"
+                            footerText={["Чего сидишь? Порадуй котэ, ", <span className='buy'>купи.</span>]}
+                            footerTextSelected="Печень утки разварная с артишоками."
+                            footerTextDisabled="Печалька, с фуа-гра закончился."
+                        />
+                        <Item
+                            headText="Сказочное заморское яство"
+                            titleName="Нямушка"
+                            titleNameWith="с рыбой"
+                            subtext1={[<span className="bold">40</span>, " порций"]}
+                            subtext2={[<span className="bold">2</span>, " мыши в подарок"]}
+                            weightCount="2"
+                            unitMeas="кг"
+                            selectedHoverText="Котэ не одобряет?"
+                            footerText={["Чего сидишь? Порадуй котэ, ", <span className='buy'>купи.</span>]}
+                            footerTextSelected="Головы щучьи с чесноком да свежайшая сёмгушка."
+                            footerTextDisabled="Печалька, с рыбой закончился."
+                        />
+                        <Item
+                            headText="Сказочное заморское яство"
+                            titleName="Нямушка"
+                            titleNameWith="с курой"
+                            subtext1={[<span className="bold">100</span>, " порций"]}
+                            subtext2={[<span className="bold">5</span>, " мышей в подарок"]}
+                            subtext3="заказчик доволен"
+                            weightCount="5"
+                            unitMeas="кг"
+                            selectedHoverText="Котэ не одобряет?"
+                            footerText={["Чего сидишь? Порадуй котэ, ", <span className='buy' >купи.</span>]}
+                            footerTextSelected="Филе из цыплят с трюфелями в бульоне."
+                            footerTextDisabled="Печалька, с курой закончился."
+                            disabled
+                        />
+                    </div>
+                </div>
             </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
--- src/item.js
+++ src/item.js
@@ -4,91 +4,88 @@ import './item.scss';
 import cat from './img/cat.png';
 
 class Item extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      selected: false,
-      hover: false
+        this.state = {
+            selected: false,
+            hover: false
+        }
     }
 
-  }
-
-  componentDidMount() {
-    ReactDOM.findDOMNode(this).addEventListener('click', this.buyClickCheck);
-    ReactDOM.findDOMNode(this).addEventListener('mouseenter', this.mouseEnterItemCheck);
-    ReactDOM.findDOMNode(this).addEventListener('mouseleave', this.mouseLeaveItemCheck);
-  }
-  componentWillUnmount() {
-    ReactDOM.findDOMNode(this).removeEventListener('click', this.buyClickCheck);
-    ReactDOM.findDOMNode(this).removeEventListener('mouseenter', this.mouseEnterItemCheck);
-    ReactDOM.findDOMNode(this).removeEventListener('mouseleave', this.mouseLeaveItemCheck);
-  }
-
-  mouseEnterItemCheck = (e) => {
-      if (e.target.classList.contains('item') && !this.state.hover && this.state.selected) {
-          if (!this.state.hover) {
-              console.log('change!');
-              this.setState(state => ({
-                  hover: true
-              }));
-          }
-      }
-  }
-
-  mouseLeaveItemCheck = (e) => {
-      if (e.target.classList.contains('item') && this.state.hover) {
-          this.setState(state => ({
-              hover: false
-          }));
-      }
-  }
-
-  buyClickCheck = (e) => {
-    if (e.target.classList.contains('buy') && !this.state.selected) {
-      this.select();
+    componentDidMount() {
+        ReactDOM.findDOMNode(this).addEventListener('click', this.buyClickCheck);
+        ReactDOM.findDOMNode(this).addEventListener('mouseenter', this.mouseEnterItemCheck);
+        ReactDOM.findDOMNode(this).addEventListener('mouseleave', this.mouseLeaveItemCheck);
     }
-  }
 
-  select = () => {
-    if (!this.props.disabled) {
-      this.setState(state => ({
-        selected: !state.selected
-      }));
-
-      if (this.state.hover) {
-          this.setState(state => ({
-              hover: false
-          }));
-      }
+    componentWillUnmount() {
+        ReactDOM.findDOMNode(this).removeEventListener('click', this.buyClickCheck);
+        ReactDOM.findDOMNode(this).removeEventListener('mouseenter', this.mouseEnterItemCheck);
+        ReactDOM.findDOMNode(this).removeEventListener('mouseleave', this.mouseLeaveItemCheck);
     }
-  }
 
-  render() {
-    return (
-      <div className={"item " + (this.props.disabled ? 'disabled' : '')}>
-        <div onClick={this.select} className={(this.state.selected ? 'selected ' + (this.state.hover ? 'selected-hover' : '') : '')}>
-          <div className="item-container">
-              <h4 className={this.state.hover ? 'hide' : 'show'}>{this.props.headText}</h4>
-              <h4 className={"selected-text " + (this.state.hover ? 'show' : 'hide')}>{this.props.selectedHoverText}</h4>
-              <h1>{this.props.titleName}</h1>
-              <h2>{this.props.titleNameWith}</h2>
-              <p>{this.props.subtext1}</p>
-              <p>{this.props.subtext2}</p>
-              <p>{this.props.subtext3}</p>
-              <img src={cat}/>
-              <div className="weight">
-                  <span className="count">{this.props.weightCount}</span>
-                  <span className="unitMeas">{this.props.unitMeas}</span>
-              </div>
-          </div>
-        </div>
-        <p className={"footer-text " + (this.state.selected || this.props.disabled ? 'hide' : 'show')}>{this.props.footerText}</p>
-        <p className={"footer-text footer-text-selected " + (this.state.selected ? 'show' : 'hide')}>{this.props.footerTextSelected}</p>
-        <p className={"footer-text footer-text-disabled " + (this.props.disabled ? 'show' : 'hide')}>{this.props.footerTextDisabled}</p>
-      </div>
-    );
-  }
+    mouseEnterItemCheck = (e) => {
+        if (e.target.classList.contains('item') && !this.state.hover && this.state.selected) {
+            this.setState(state => ({
+                hover: true
+            }));
+        }
+    }
+
+    mouseLeaveItemCheck = (e) => {
+        if (e.target.classList.contains('item') && this.state.hover) {
+            this.setState(state => ({
+                hover: false
+            }));
+        }
+    }
+
+    buyClickCheck = (e) => {
+        if (e.target.classList.contains('buy') && !this.state.selected) {
+            this.select();
+        }
+    }
+
+    select = () => {
+        if (!this.props.disabled) {
+            this.setState(state => ({
+                selected: !state.selected
+            }));
+
+            if (this.state.hover) {
+                this.setState(state => ({
+                    hover: false
+                }));
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className={"item " + (this.props.disabled ? 'disabled' : '')}>
+                <div onClick={this.select} className={(this.state.selected ? 'selected ' + (this.state.hover ? 'selected-hover' : '') : '')}>
+                    <div className="item-container">
+                        <h4 className={this.state.hover ? 'hide' : 'show'}>{this.props.headText}</h4>
+                        <h4 className={"selected-text " + (this.state.hover ? 'show' : 'hide')}>{this.props.selectedHoverText}</h4>
+                        <h1>{this.props.titleName}</h1>
+                        <h2>{this.props.titleNameWith}</h2>
+                        <p>{this.props.subtext1}</p>
+                        <p>{this.props.subtext2}</p>
+                        <p>{this.props.subtext3}</p>
+                        <img src={cat}/>
+                        <div className="weight">
+                            <span className="count">{this.props.weightCount}</span>
+                            <span className="unitMeas">{this.props.unitMeas}</span>
+                        </div>
+                    </div>
+                </div>
+                <p className={"footer-text " + (this.state.selected || this.props.disabled ? 'hide' : 'show')}>{this.props.footerText}</p>
+                <p className={"footer-text footer-text-selected " + (this.state.selected ? 'show' : 'hide')}>{this.props.footerTextSelected}</p>
+                <p className={"footer-text footer-text-disabled " + (this.props.disabled ? 'show' : 'hide')}>{this.props.footerTextDisabled}</p>
+            </div>
+        );
+    }
 }
 
 export default Item;
